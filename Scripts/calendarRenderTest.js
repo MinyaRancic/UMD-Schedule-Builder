0248176ff7975ab5ccff0d84271e715b9690c035
unknown <rancic.minya@gmail.com>
2017-11-26 22:55:34 -0500

    Testing the weekCalendar.js library

---
+++ Scripts/calendarRenderTest.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#calendar').weekCalendar({
+    events:[{"id":10182,
+      "start":"2009-05-03T12:15:00.000+10:00",
+      "end":"2009-05-03T13:15:00.000+10:00",
+      "title":"Lunch with Mike"
+    }, {
+      "id":10182,
+      "start":"2009-05-03T14:00:00.000+10:00",
+      "end":"2009-05-03T15:00:00.000+10:00",
+      "title":"Dev Meeting"
+    }]
+  });
+});
